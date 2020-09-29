3983d42cf14e384f4ac3a4476983c7b21dc08a7d
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-09-29 21:01:58 +0300

    preparing script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-/* JS goes here */
+const imageCount = 10;
+const apiKey = '';
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${imageCount}`;
+
+function getPhotos() {
+    fetch(apiURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+        })
+        .catch(function (error) {
+
+        })
+}
+
+getPhotos();
