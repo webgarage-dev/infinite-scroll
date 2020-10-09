ea2cc310a13f562c1618981dcac5b9f1ff0a5515
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-10-09 18:44:40 +0300

    getting JSON from unsplash

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ const loader = document.getElementById("loader");
 /* Global Photo Array */
 let photosArray = [];
 
+/* Create elements for links and photos and add to DOM */
+function displayPhotos() {
+    photosArray.forEach(photo => {
+        const item = document.createElement("a");
+        item.setAttribute("href", photo.links.html);
+        item.setAttribute("target", "_blank");
+        const img = document.createElement("img");
+        img.setAttribute("src", photo.urls.regular);
+        img.setAttribute("alt", photo.alt_description);
+        img.setAttribute("title", photo.alt_description);
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+/* Get photos from Unsplash API */
 function getPhotos() {
     fetch(apiURL)
         .then(function (response) {
@@ -15,9 +31,10 @@ function getPhotos() {
         })
         .then(function (data) {
             photosArray = data;
+            displayPhotos();
         })
         .catch(function (error) {
-
+            console.log("Something goes wrong..."+error);
         })
 }
 
