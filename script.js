13f37d6e0fe5908f0c480f22b92549c35512e8dc
goodhousekeeper <2949865+goodhousekeeper@users.noreply.github.com>
2020-10-09 18:22:18 +0300

    getting JSON from unsplash

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 /* Uses private API */
 const apiURL = `https://api.webgarage.dev/uninfscroll/photos/get`;
 
+/* HTML elements*/
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+/* Global Photo Array */
+let photosArray = [];
+
 function getPhotos() {
     fetch(apiURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-
+            photosArray = data;
         })
         .catch(function (error) {
 
